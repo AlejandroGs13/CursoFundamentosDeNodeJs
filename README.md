4cbe33e4e5418cdeec74764cbbcd274698238c85
AlejandroGs13 <alejandrogs13@outlook.com>
2020-08-20 00:25:51 -0500

    conceptos asincronismo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CursoFundamentosDeNodeJs
 Curso platzi Fundamentos de node js
+
+
+Apuntes:
+
+* Problemas de que sea mono hilo al encontrar un error para todo el programa
---
+++ conceptos/monohilo.js
@@ -0,0 +1,14 @@
+console.log('Hola mundo!');
+
+// ejecuta una accion dependiendo el tiempo establecido
+let i = 0;
+setInterval(()=>{
+console.log(i);
+i++;
+    // if(i === 5){
+    //     var a = 3 + z;
+    // }
+},1000);
+
+
+console.log('Segunda instriccion');
